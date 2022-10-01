86b0268d8358be3fdf6915c884d3ae19afbefc19
Ivan Nouel <61523959+nouelivan@users.noreply.github.com>
2022-09-30 21:46:30 -0400

    Create FrogJmp.py

---
+++ FrogJmp.py
@@ -0,0 +1,9 @@
+# Given three integers X, Y and D, this fucntion returns the minimal number of jumps a from must perform to get from position X to a position equal to or greater than Y.
+
+def solution(X, Y, D):
+    if X == Y:
+        return 0
+    elif (Y - X) % D == 0:
+        return int((Y - X) / D)
+    else:
+        return int((Y - X) // D + 1)
